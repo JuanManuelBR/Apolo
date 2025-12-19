ff3519c12c082850ff218874736e02e946b6101e
Juan Blandón <2420211018@estudiantesunibague.edu.co>
2025-12-19 09:56:13 -0500

    Update UserRoutes.ts

--- server/Users/src/routes/UserRoutes.ts
+++ server/Users/src/routes/UserRoutes.ts
@@ -8,13 +8,7 @@ router.post("/logout", UserController.logout);
 router.post("/", UserController.AddUser);
 router.use(authMiddleware);
 router.get("/", UserController.getUsers);
-
 router.get("/:id", UserController.getUserById);
-
-
-
 router.delete("/:id", UserController.deleteUser);
-
 router.put("/:id", UserController.editUser);
-
 export default router;
